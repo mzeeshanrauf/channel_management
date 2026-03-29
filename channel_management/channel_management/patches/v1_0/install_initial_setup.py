a58c1761f46e2bc6d2c4c1c7aed92be30e360e06
mzeeshanrauf <zeeshanrauf@outlook.com>
2026-03-29 21:34:18 +0300

    fixes

--- channel_management/channel_management/patches/v1_0/install_initial_setup.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-
-def execute():
-    from channel_management.channel_management.install import create_roles, create_customer_custom_fields
-    create_roles()
-    create_customer_custom_fields()
-    frappe.db.commit()
-    print("  ✅ Channel Management v1.0 patch applied.")
--- cm_fixed/channel_management/report/sales_margin_report/sales_margin_report.js
+++
@@ -1,9 +0,0 @@
-frappe.query_reports["Sales Margin Report"] = {
-	filters: [
-		{fieldname: "from_date",    label: __("From Date"),    fieldtype: "Date",   default: frappe.datetime.month_start()},
-		{fieldname: "to_date",      label: __("To Date"),      fieldtype: "Date",   default: frappe.datetime.get_today()},
-		{fieldname: "customer",     label: __("Customer"),     fieldtype: "Link",   options: "Customer"},
-		{fieldname: "sales_person", label: __("Sales Person"), fieldtype: "Link",   options: "Sales Person"},
-		{fieldname: "plan",         label: __("Plan"),         fieldtype: "Link",   options: "Plan Master"},
-	],
-};
