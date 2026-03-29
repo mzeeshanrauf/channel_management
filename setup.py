f3c12f69b3f779f79b76d936526701298fbbdf0a
mzeeshanrauf <zeeshanrauf@outlook.com>
2026-03-29 21:18:28 +0300

    fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
+
 with open("requirements.txt") as f:
     install_requires = f.read().strip().split("\n")
+
 setup(
     name="channel_management",
     version="1.0.0",
